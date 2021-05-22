d251ced6915e0b40479df80310e9b252497c397a
nerblock <>
2021-05-22 09:21:42 +0200

    support for new 444 engine

--- 444hu_comments.js
+++ 444hu_comments.js
@@ -15,8 +15,12 @@ document.addEventListener('DOMContentLoaded', function () {
             "sites":{
                 "default": {
                     "top_button_insert_selector": "div.byline",
-                    "comments_section_html": getCommentsInnerHTML(),
-                    "init_script": function() { return ""; }
+                    "comments_section_insert_selector": "article footer.hide-print",
+                    "comments_section_insert_retry": false,
+                    "comments_section_html": getCommentsInnerHTMLBlog(),
+                    "init_script": function() {
+                        return "require('blog/comment').default();";
+                    }
                 },
                 "jo.444.hu": {
                     "button_text_color": "#222"
@@ -25,11 +29,32 @@ document.addEventListener('DOMContentLoaded', function () {
                     "comments_section_html": getCommentsInnerHTMLGeekz(),
                     "init_script": function() {
                         if (new Date(document.querySelector("meta[itemprop='dateCreated']").getAttribute('content')).getTime() > 1453379951779) {
-                            return "window.disqus_shortname = 'geekzblog';";
+                            return "require('blog/comment').default(); window.disqus_shortname = 'geekzblog';";
                         }
                         return "";
                     }
-                }
+                },
+                "444.hu": {
+                    "top_button_insert_selector": '[style="--avatar-width: 40px; --avatar-height: 40px;"]',
+                    "top_button_insert_parent": true,
+                    "comments_section_html": getCommentsInnerHTML(),
+                    "comments_section_insert_selector": "#ap-article-footer1",
+                    "comments_section_insert_parent": true,
+                    "comments_section_insert_retry": true,
+                    "init_script": function() { return `
+                        document.querySelector(".comments-toggle").onclick = function() {
+                            window.disqus_url = document.URL;
+                            let _dc = require('disqus/components/disqus-comments');
+                            let _lc = new _dc.default();
+                            let _go = Ember.getOwner;
+                            Ember.getOwner = function() { return { lookup: function() { return { get: function() { return "444hu"; }}}}}
+                            _lc.args = {};
+                            _lc.loadComments.perform();
+                            Ember.getOwner = _go;
+                            this.remove();
+                        }
+                    `;}
+                },
             }
         }
         if (null !== key) return cfg[key];
@@ -54,7 +79,9 @@ document.addEventListener('DOMContentLoaded', function () {
             if (null !== document.querySelector(".comments-toggle")) document.querySelector(".comments-toggle").click();
         }
 
-        var bl = document.querySelector(getCurrentHostConfigFor("top_button_insert_selector"));
+
+        let bl = getCurrentHostConfigFor("top_button_insert_parent") ? document.querySelector(getCurrentHostConfigFor("top_button_insert_selector")).parentElement : document.querySelector(getCurrentHostConfigFor("top_button_insert_selector"));
+
         if (null !== bl) {
             bl.innerHTML = '<div><button class="gae-comment-click-open comments-toggle-top">Hozzászólások</button></div>' + bl.innerHTML;
             document.querySelector('.comments-toggle-top').style.color = getCurrentHostConfigFor("button_text_color");
@@ -104,12 +131,12 @@ document.addEventListener('DOMContentLoaded', function () {
     function initCommentButton() {
         // init comments button after load
         var script = document.createElement('script');
-        script.textContent =
-        `window.addEventListener('load', () => {
-            require('blog/comment').default();
-            ` + getCurrentHostConfigFor("init_script")() + `
+        script.textContent = `
+            //window.addEventListener('pageshow', () => {
+                ` + getCurrentHostConfigFor("init_script")() + `
+            //});
             console.debug('` + log("initialized", true) + `');
-        });`;
+        `;
         (document.head || document.documentElement).prepend(script);
     }
 
@@ -142,6 +169,30 @@ document.addEventListener('DOMContentLoaded', function () {
         </div>`;
         return html;
     }
+    function getCommentsInnerHTMLBlog() {
+        var html =
+        `<!-- comments -->
+        <div class="comments-docked-resizer"><div></div></div>
+        <div class="subhead">` +
+            `<span class="logo" title="` + chrome.runtime.getManifest().short_name + ` v` + chrome.runtime.getManifest().version + `"><span class="comments-docked-title">Hozzászólások</span></span>` +
+            `<span class="comments-title">Uralkodj magadon!</span>` +
+            `<span class="comments-docked-toggle">` + 
+                `<span class="comments-docked-open"><button>Hozzászólások oldalsáv</button></span>` +
+                `<span class="comments-docked-close comments-docked-hidden"><a><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAsAAAALCAYAAACprHcmAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAADxJREFUeNpi+A8BaUDMgAeD5BEMPBrg8gwENKCI45TAZgADHpMwbMLrRnQ5sk0m2s1EhwZJ4Ux0DAIEGABDKYzoRdlxEwAAAABJRU5ErkJggg=="></a></span>` +
+            `</span>
+        </div>
+        <div class="comments-contents">
+            <div>
+                <b>Új kommentelési szabályok érvényesek 2019. december 2-től.</b>
+                <a href="https://444.hu/2019/12/02/valtoznak-a-kommenteles-szabalyai-a-444-en" target="_blank">Itt olvashatod el</a>, hogy mik azok, és <a href="https://444.hu/2019/12/02/ezert-valtoztatunk-a-kommenteles-szabalyain" target="_blank">itt azt</a>, hogy miért vezettük be őket.
+            </div>
+            <button class="gae-comment-click-open comments-toggle">Hozzászólások</button>
+            <div class="ad"><div id="444_aloldal_kommentek"></div></div>
+            <div id="disqus_thread" class="freehand layout"></div>
+        </div>`;
+        return html;
+    }
+
     function getCommentsInnerHTML() {
         var html =
         `<!-- comments -->
@@ -166,18 +217,50 @@ document.addEventListener('DOMContentLoaded', function () {
         return html;
     }
 
-    var af = document.querySelector("article footer.hide-print");
-    if (null === af) {
-        // only run on article pages
-        log("no article found, doing nothing");
-    } else {
-        if (null !== document.getElementById("disqus_thread")) {
-            log("comments enabled by 444.hu");
-            document.getElementById("comments").innerHTML = getCurrentHostConfigFor("comments_section_html");
-        } else {
-            log("comments disabled by 444.hu");
-            af.innerHTML += '<section id="comments">' + getCommentsInnerHTML() + '</section>';
-        }
-        init();
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
     }
+      
+    async function start() {
+        let af;
+        let i = 10;
+        while (1) {
+            af = document.querySelector(getCurrentHostConfigFor("comments_section_insert_selector"));
+            if (null !== af || !getCurrentHostConfigFor("comments_section_insert_retry")) {
+                //log(af);
+                await sleep(1000);
+                break;
+            }
+            if (i > 0) {
+                log("sleeping");
+                await sleep(200);
+                i--;
+            } else {
+                break;
+            }
+        }
+    
+        if (null === af) {
+            // only run on article pages
+            log("no article found, doing nothing");
+        } else {
+            if (null !== document.getElementById("disqus_thread")) {
+                log("comments enabled by 444.hu");
+                document.getElementById("comments").innerHTML = getCurrentHostConfigFor("comments_section_html");
+            } else {
+                log("comments disabled by 444.hu");
+                if (getCurrentHostConfigFor("comments_section_insert_parent")) {
+                    af = document.querySelector(getCurrentHostConfigFor("comments_section_insert_selector"));
+                    af = af.parentElement;
+                    af.innerHTML = '<section id="comments">' + getCurrentHostConfigFor("comments_section_html") + '</section>' + af.innerHTML;
+                } else {
+                    af = document.querySelector(getCurrentHostConfigFor("comments_section_insert_selector"));
+                    af.innerHTML += '<section id="comments">' + getCurrentHostConfigFor("comments_section_html") + '</section>';
+                }
+            }
+            init();
+        }
+    }
+
+    start();
 });
