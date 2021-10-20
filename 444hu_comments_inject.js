1c1d71febc36be7cac188f5e1a2429c69e000814
nerblock <>
2021-10-20 02:47:37 +0200

    sidebar

--- 444hu_comments_inject.js
+++ 444hu_comments_inject.js
@@ -42,9 +42,9 @@
                 <div class="ext-wrapper">
                     <div class="wrapper">
                         <div class="version">444hsz bővítmény verzió<span>` + _version + `</span></div>
-                        <div class="slider-switch-wrapper"><label class="slider-switch" for="autoloadToggle">Kommentek auto-betöltése<input type="checkbox" id="autoloadToggle"><span class="slider round"></span></label></div>
-                        <div class="slider-switch-wrapper"><label class="slider-switch" for="rulesToggle">Szolgálati közlemény<input type="checkbox" id="rulesToggle" checked><span class="slider round"></span></label></div>
-                        <div class="slider-switch-wrapper"><label class="slider-switch" for="recommendationsToggle">Disqus ajánlások<input type="checkbox" id="recommendationsToggle"><span class="slider round"></span></label></div>
+                        <div class="slider-switch-wrapper"><label class="slider-switch" for="autoloadToggle">Kommentek auto betöltése<input type="checkbox" id="autoloadToggle"><span class="slider"></span></label></div>
+                        <div class="slider-switch-wrapper"><label class="slider-switch" for="rulesToggle">Szolgálati közlemény<input type="checkbox" id="rulesToggle" checked><span class="slider"></span></label></div>
+                        <div class="slider-switch-wrapper"><label class="slider-switch" for="recommendationsToggle">Disqus ajánlások<input type="checkbox" id="recommendationsToggle"><span class="slider"></span></label></div>
                         <div class="slider-switch-wrapper">
                             <label class="" for="userForumShortName">Nem hivatalos Disqus fórum<span><input type="text" id="userForumShortName" placeholder="444hsz"></span></label>
                         </div>
@@ -66,17 +66,6 @@
         `<div class="comments-wrapper">` +
         `<section id="comments">` +
         `<div class="comments-docked-resizer"><div></div></div>
-        <div class="subhead">` +
-            `<span class="logo" title=""><span class="comments-docked-title">Kommentek</span></span>` +
-            `<span class="comments-title">Kommentek</span>` +
-            `<span class="comments-docked-toggle">` + 
-                `<span class="comments-docked-open">
-                    <button id="settingsToggle" title="Beállítások"><svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" viewBox="0 0 24 24"><path d="M24 13.616v-3.232c-1.651-.587-2.694-.752-3.219-2.019v-.001c-.527-1.271.1-2.134.847-3.707l-2.285-2.285c-1.561.742-2.433 1.375-3.707.847h-.001c-1.269-.526-1.435-1.576-2.019-3.219h-3.232c-.582 1.635-.749 2.692-2.019 3.219h-.001c-1.271.528-2.132-.098-3.707-.847l-2.285 2.285c.745 1.568 1.375 2.434.847 3.707-.527 1.271-1.584 1.438-3.219 2.02v3.232c1.632.58 2.692.749 3.219 2.019.53 1.282-.114 2.166-.847 3.707l2.285 2.286c1.562-.743 2.434-1.375 3.707-.847h.001c1.27.526 1.436 1.579 2.019 3.219h3.232c.582-1.636.75-2.69 2.027-3.222h.001c1.262-.524 2.12.101 3.698.851l2.285-2.286c-.744-1.563-1.375-2.433-.848-3.706.527-1.271 1.588-1.44 3.221-2.021zm-12 2.384c-2.209 0-4-1.791-4-4s1.791-4 4-4 4 1.791 4 4-1.791 4-4 4z"/></svg></button>
-                    <button id="sidebarToggle" title="Oldalsáv"><svg class="flipped" height="19px" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g fill="#ffffff" fill-rule="nonzero"><path d="M19.25,4 C20.7688,4 22,5.23122 22,6.75 L22,6.75 L22,17.25 C22,18.7688 20.7688,20 19.25,20 L19.25,20 L4.75,20 C3.23122,20 2,18.7688 2,17.25 L2,17.25 L2,6.75 C2,5.23122 3.23122,4 4.75,4 L4.75,4 Z M19.25,5.5 L9,5.5 L9,18.5 L19.25,18.5 C19.9404,18.5 20.5,17.9404 20.5,17.25 L20.5,6.75 C20.5,6.05964 19.9404,5.5 19.25,5.5 Z"></path></g></g></svg></button>
-                </span>` +
-                `<span class="comments-docked-close comments-docked-hidden"><a><img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAsAAAALCAYAAACprHcmAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJbWFnZVJlYWR5ccllPAAAADxJREFUeNpi+A8BaUDMgAeD5BEMPBrg8gwENKCI45TAZgADHpMwbMLrRnQ5sk0m2s1EhwZJ4Ux0DAIEGABDKYzoRdlxEwAAAABJRU5ErkJggg=="></a></span>` +
-            `</span>
-        </div>
         <div class="comments-contents">
             <div class="forum-rules">
                 <div title="Bezár" class="close-button"><svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 0 24 24" width="20px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12 19 6.41z"/></svg></div>
@@ -229,11 +218,17 @@
         function onClickSidebarToggle() {
             //document.querySelector(".comments-settings").classList.add('collapse');
             //document.querySelector("div#comments_tabs .titlebar button.button-settings").classList.remove('active');
-            document.getElementById('comments').classList.toggle('docked-comments');
-            document.querySelector('.comments-docked-open').classList.toggle('comments-docked-hidden');
-            document.querySelector('.comments-docked-close').classList.toggle('comments-docked-hidden');
+
+            document.getElementById('comments_wrapper').classList.toggle('sidebar');
+            document.querySelector("div#comments_tabs .titlebar button.button-sidebar").classList.toggle('active');
+
+
+            //document.querySelector('.comments-docked-open').classList.toggle('comments-docked-hidden');
+            //document.querySelector('.comments-docked-close').classList.toggle('comments-docked-hidden');
             document.getElementById('comments').style.width = 'var(--docked-comments-width)';
-            document.querySelector("section#comments .comments-docked-resizer").style.right = "calc(var(--docked-comments-width) - var(--docked-comments-resizer-width))";
+
+            document.querySelector("#comments_wrapper .comments-docked-resizer").style.right = "calc(var(--docked-comments-width) - var(--docked-comments-resizer-width))";
+
             let el = document.querySelector(".comments-toggle");
             if (null !== el) el.click();
         }
@@ -364,21 +359,11 @@
             document.querySelector(".comments-toggle-top").onclick = onClickTopCommentsButton;
         }
 
-        document.querySelector('.comments-docked-open>button#sidebarToggle').onclick = onClickSidebarToggle;
-        document.querySelector('.comments-docked-close>a').onclick = onClickSidebarToggle;
-
-        //document.querySelector('.comments-settings .close-button').onclick = onClickToggleSettings;
-        document.querySelector('.comments-docked-open>button#settingsToggle').onclick = onClickToggleSettings;
-
-
         document.querySelector('#comments_tabs .titlebar button.button-sidebar').onclick = onClickSidebarToggle;
         document.querySelector('#comments_tabs .titlebar button.button-settings').onclick = onClickToggleSettings;
 
         document.getElementById('tab-user').onclick = onClickForumUser;
         document.getElementById('tab-444hu').onclick = onClickForum444hu;
-
-        //document.querySelector('.comments-docked-open input#forumToggle').onclick = onClickForumToggle;
-        //document.querySelector('.comments-docked-open input#forumToggle').checked = !(_currentForumShortName === _defaultForumShortName);
 
         document.querySelector('.comments-contents .forum-rules .close-button').onclick = onClickCloseRules;
         document.querySelector('.comments-contents .forum-rules .text-close-button').onclick = onClickCloseRules;
